8038b3f223067169a3b7b2db4cc6729839e71239
ashokmoorthi <ashokmoorthims@gmail.com>
2016-05-15 13:39:09 -0400

    Files updated for old scenarios

---
+++ Tryexcept/tryexcept_Ashok.py
@@ -0,0 +1,23 @@
+''' Ram's scenario:
+1) Get two input
+2) Give the value 0 for Denominator
+3) Handle the error
+4) Next step handle the other errors (Like if we give string instead of numeric)
+5) How to handle unknown error with unknown resolution
+'''
+import sys
+#while True:
+try:
+    n = input("Numerator:")
+    d = input("Denominator:")
+    print (int(n) / int(d))
+#        break
+#    except ZeroDivisionError:
+#        print ("Division by Zero error")
+#        break
+#    except ValueError:
+#        print ("Only Numeric Values please")
+#        break
+except:
+    print ("Unexpected error:", sys.exc_info()[0])
+    #        break
