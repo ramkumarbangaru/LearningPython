f434d70005b2b834e1e263319798c4b85e083bb3
bacharya <biswapati.lipun@gmail.com>
2016-06-15 17:11:39 -0400

    Adding Paramiko scenario's solution

--- 8.Paramiko/paramiko_shell.py
+++ 8.Paramiko/paramiko_shell.py
@@ -2,19 +2,31 @@ import paramiko
 import time
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect("172.17.240.11",port=22,username='idirect',password='iDirect')
+ssh.connect("192.168.56.101",port=22,username='bacharya',password='iDirect')
 shell = ssh.invoke_shell()
+
+log = open("C:\\python_test\\abc.txt","w")
 # shell.send("su - \n")
 # shell.send("iDirect\n")
 shell.send("cd /tmp/ \n")
-#time.sleep(1)
-shell.send("ls -ltr \n")
+output = ''
 time.sleep(1)
-while not shell.recv_ready():
-    time.sleep(2)
-results = ''
-while shell.recv_ready():
-    results = results + shell.recv(1).decode("utf-8")
-# shell.send("su -n")
-# shell.send("su -n")
-print(results)
+if shell.recv_ready():
+    output = shell.recv(9999)
+
+for ln in output.decode("utf-8").split('\n'):
+    print (ln)
+    log.writelines(ln)
+shell.close()
+ssh.close()
+# #time.sleep(1)
+# shell.send("ls -ltr \n")
+# time.sleep(1)
+# while not shell.recv_ready():
+#     time.sleep(2)
+# results = ''
+# while shell.recv_ready():
+#     results = results + shell.recv(1).decode("utf-8")
+# # shell.send("su -n")
+# # shell.send("su -n")
+# print(results)
