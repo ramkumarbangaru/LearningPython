94f45138053ad8ddb373e8e5b6c02d94429b9746
biswapatiacharya <biswapati.lipun@gmail.com>
2016-06-23 01:22:38 -0400

    test2

---
+++ 9.Paramiko_with_Validation/validatition.py
@@ -0,0 +1,36 @@
+import paramiko
+import time
+
+def sendxcmd(shell,cmd):
+    shell.send(cmd + "\n")
+    while not shell.recv_ready():
+        time.sleep(0.5)
+    output = shell.recv(9999)
+    return output.decode("utf-8")
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect("192.168.56.101",port=22,username='bak',password='iDirect')
+shell = ssh.invoke_shell()
+log = open("C:\\python_test\\abc.txt","w")
+sendxcmd(shell,"cd /tmp")
+# log.write(sendxcmd(shell,"ll"))
+log.write(sendxcmd(shell,"/bin/ls -ls | awk \'{print $10}\'"))
+shell.close()
+log.close()
+
+
+file1 = open("C:\\python_test\\abc.txt","r")
+list1 = file1.readline().split("\n")
+# list2 = list1.decode("utf-8")
+
+print(list1)
+
+# for line in list1:
+#     if line == "temp.txt":
+#         print("line")
+    # else:
+    #     print("FALSE")
+
+
+# shell.send("rm -rf test\n")
