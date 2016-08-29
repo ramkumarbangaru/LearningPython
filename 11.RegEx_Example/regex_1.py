b3edf47298d122250a619c4c2b57121eb6842558
ashokmoorthi <ashokmoorthims@gmail.com>
2016-08-29 18:32:18 -0400

    Minor Updates with comments in program

--- 11.RegEx_Example/regex_1.py
+++ 11.RegEx_Example/regex_1.py
@@ -1,7 +1,11 @@
+# Program to validate the given input is from the range of -35.5 to -65.5
+
 import re
 print("Enter the number please:")
 a=input()
 # b= re.match(r'-\d{2}.\d{2}',a)
 b= re.match(r'^-[3][5].[5-9]|^-3[6-9].[0-9]|^-[4-5][0-9].[0-9]|^-6[0-4].[0-9]|^-6[5].[0-5]',a)
-
-print(b)
+if b == None:
+    print ("Entered value is not between -35.5 to -65.5 range:", a)
+else:
+    print ("Entered value is between -35.5 to -65.5 range:", a)
