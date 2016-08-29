1d440116f3fba2a09ca273b70cd96b9de8014f48
ashokmoorthi <ashokmoorthims@gmail.com>
2016-08-29 19:06:25 -0400

    Update regex_1.py

--- 11.RegEx_Example/regex_1.py
+++ 11.RegEx_Example/regex_1.py
@@ -5,6 +5,7 @@ print("Enter the number please:")
 a=input()
 # b= re.match(r'-\d{2}.\d{2}',a)
 b= re.match(r'^-[3][5].[5-9]|^-3[6-9].[0-9]|^-[4-5][0-9].[0-9]|^-6[0-4].[0-9]|^-6[5].[0-5]',a)
+print (b)
 if b == None:
     print ("Entered value is not between -35.5 to -65.5 range:", a)
 else:
