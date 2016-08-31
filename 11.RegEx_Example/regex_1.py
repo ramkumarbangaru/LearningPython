4fb79ed1b6555d4ac140178ddfde49a4d057f6a5
ashokmoorthi <ashokmoorthims@gmail.com>
2016-08-31 14:11:19 -0400

    Update regex_1.py

--- 11.RegEx_Example/regex_1.py
+++ 11.RegEx_Example/regex_1.py
@@ -3,9 +3,7 @@ import re
 # a=input()
 # # b= re.match(r'-\d{2}.\d{2}',a)
 # b= re.match(r'^-[3][5].[5-9]|^-3[6-9].[0-9]|^-[4-5][0-9].[0-9]|^-6[0-4].[0-9]|^-6[5].[0-5]',a)
-#
 # print(b)
-
 c = re.match("(\w{3})\d{2}(\w{4})","Ram20abcd")
 print(c.group(0))
 print(c.group(1))
